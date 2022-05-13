6dab219d25e448eea0db755b3238ac0aabbcc81d
Honchar Yaroslav <honchar.yara@gmail.com>
2022-05-13 10:53:59 +0300

    section: our-products(no slider)

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,17 @@
 import 'bootstrap/dist/js/bootstrap.bundle';
+const bootstrap = require('bootstrap');
 import refs from './refs';
 
 refs.navCloseBtn.addEventListener('click', () => {
-    setTimeout(() => {
-        refs.navbar.removeAttribute('style');
-    }, 500)
-})
+  setTimeout(() => {
+    refs.navbar.removeAttribute('style');
+  }, 500);
+});
+
+var myCarousel = document.querySelector('#our-productions__slider');
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false,
+});
+
+console.dir(carousel);
